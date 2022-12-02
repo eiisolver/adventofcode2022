b1e2804286574559ef0ca61c9900762ee87e68c0
Louis Verhaard <louis@shortestpath.se>
2022-12-02 22:43:18 +0100

    Day 2

---
+++ 2.py
@@ -0,0 +1,7 @@
+games = [(ord(line[0]) - ord('A'), ord(line[2]) - ord('X')) for line in open("2_input.txt", 'r')]
+
+part1 = sum(1 + game[1] + [3, 6, 0, 6, 0][game[1] - game[0]] for game in games)
+part2 = sum((2 + game[0] + game[1]) % 3  + 1 + 3 * game[1] for game in games)
+
+print("part 1:", part1)
+print("part 2:", part2)
