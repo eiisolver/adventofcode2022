a7708ff5667eefe7c4ad6e66b286edad97072857
Louis Verhaard <louis@shortestpath.se>
2022-12-04 10:05:19 +0100

    Day 4

---
+++ 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream f("4_input.txt");
+    int a, b, c, d;
+    char h;
+    int overlap = 0;
+    int full_overlap = 0;
+
+    while (f >> a >> h >> b >> h >> c >> h >> d) {
+        if (a <= d && c <= b) {
+            ++overlap;
+            if ((a >= c && b <= d) || (a <= c && b >= d)) {
+                ++full_overlap;
+            }
+        }
+    }
+    cout << "Part 1: " << full_overlap << "\n";
+    cout << "Part 2: " << overlap << "\n";
+}
